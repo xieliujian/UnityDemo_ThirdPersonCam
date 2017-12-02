18fb4c7a3246e1be9dc17c3997f6ff587c3b4002
谢刘建 <377624289@qq.com>
2017-12-02 13:44:46 +0800

    增加碰撞和手势优化

--- Assets/Scripts/JoystickUI.cs
+++ Assets/Scripts/JoystickUI.cs
@@ -12,6 +12,12 @@ public class InputMgrName
 
 public class JoystickUI : MonoBehaviour
 {
+    #region 常量
+
+    public Vector3 DRAGING_BOXCOLLI_SIZE = new Vector3(900.0f, 900.0f, 1.0f);
+
+    #endregion
+
     #region 变量
 
     /// <summary>
@@ -25,9 +31,19 @@ public class JoystickUI : MonoBehaviour
     public UISprite mTouch;
 
     /// <summary>
+    /// 用来解决一边移动，一个手指有时候不能转镜头的bug(左手手指在触摸区外面的情况)
+    /// </summary>
+    public BoxCollider mTouchBoxColli;
+
+    /// <summary>
     /// 移动事件
     /// </summary>
     public Action<Vector2> OnMove;
+
+    /// <summary>
+    /// 移动开始事件
+    /// </summary>
+    public Action OnMoveStart;
 
     /// <summary>
     /// 移动结束事件
@@ -38,11 +54,21 @@ public class JoystickUI : MonoBehaviour
     /// 半径
     /// </summary>
     private float mRadius;
-    
+
+    /// <summary>
+    /// box colli初始化尺寸
+    /// </summary>
+    private Vector3 mTouchBoxColliInitSize;
+
     /// <summary>
     /// 是否触摸
     /// </summary>
     private bool mOnTouch = false;
+
+    /// <summary>
+    /// 旧的临时位置
+    /// </summary>
+    private Vector2 mOldTmpAxis = Vector2.zero;
 
     #endregion
 
@@ -52,11 +78,10 @@ public class JoystickUI : MonoBehaviour
     void Start ()
     {
         UIEventListener.Get(mTouch.gameObject).onPress = OnTouchPress;
-        UIEventListener.Get(mTouch.gameObject).onDragStart = OnTouchDragStart;
         UIEventListener.Get(mTouch.gameObject).onDrag = OnTouchDrag;
-        UIEventListener.Get(mTouch.gameObject).onDragEnd = OnTouchDragEnd;
 
         mRadius = mBg.width * 0.5f;
+        mTouchBoxColliInitSize = mTouchBoxColli.size;
     }
 	
 	// Update is called once per frame
@@ -69,22 +94,11 @@ public class JoystickUI : MonoBehaviour
 
     #region 回调函数
 
-    private void OnTouchDragStart(GameObject go)
-    {
-        mOnTouch = true;
-    }
-
     private void OnTouchDrag(GameObject go, Vector2 delta)
     {
         mOnTouch = true;
         mTouch.transform.localPosition += new Vector3(delta.x, delta.y, 0.0f);
         mTouch.transform.localPosition = Vector3.ClampMagnitude(mTouch.transform.localPosition, GetRadius());
-    }
-
-    private void OnTouchDragEnd(GameObject go)
-    {
-        mOnTouch = false;
-        mTouch.transform.localPosition = Vector2.zero;
     }
 
     private void OnTouchPress(GameObject go, bool state)
@@ -93,6 +107,21 @@ public class JoystickUI : MonoBehaviour
         if (!state)
         {
             mTouch.transform.localPosition = Vector2.zero;
+        }
+
+        if (state)
+        {
+            mTouchBoxColli.size = DRAGING_BOXCOLLI_SIZE;
+
+            if (OnMoveStart != null)
+                OnMoveStart();
+        }
+        else
+        {
+            mTouchBoxColli.size = mTouchBoxColliInitSize;
+
+            if (OnMoveEnd != null)
+                OnMoveEnd();
         }
     }
 
@@ -103,13 +132,28 @@ public class JoystickUI : MonoBehaviour
     private void UpdateJoystick()
     {
         Transform touchtrans = mTouch.transform;
+        Vector2 tempaxis = touchtrans.localPosition / GetRadius();
         if (!mOnTouch)
         {
             float x = Input.GetAxis(InputMgrName.Horizontal);
             float y = Input.GetAxis(InputMgrName.Vertical);
 
-            touchtrans.localPosition = Vector2.zero;
+            if ((x != 0.0f || y != 0.0f) && (mOldTmpAxis.x == 0.0f && mOldTmpAxis.y == 0.0f))
+            {
+                mTouchBoxColli.size = DRAGING_BOXCOLLI_SIZE;
 
+                if (OnMoveStart != null)
+                    OnMoveStart();
+            }
+            else if ((x == 0.0f && y == 0.0f) && (mOldTmpAxis.x != 0.0f || mOldTmpAxis.y != 0.0f))
+            {
+                mTouchBoxColli.size = mTouchBoxColliInitSize;
+
+                if (OnMoveEnd != null)
+                    OnMoveEnd();
+            }
+
+            touchtrans.localPosition = Vector2.zero;
             if (x != 0.0f)
             {
                 touchtrans.localPosition = new Vector2(GetRadius() * x, touchtrans.localPosition.y);
@@ -121,15 +165,11 @@ public class JoystickUI : MonoBehaviour
             }
 
             touchtrans.localPosition = Vector3.ClampMagnitude(touchtrans.localPosition, GetRadius());
+            tempaxis = touchtrans.localPosition / GetRadius();
+            mOldTmpAxis = new Vector2(x, y);
         }
 
-        Vector2 tempaxis = touchtrans.localPosition / GetRadius();
-        if (tempaxis == Vector2.zero)
-        {
-            if (OnMoveEnd != null)
-                OnMoveEnd();
-        }
-        else if (tempaxis.x != 0.0f || tempaxis.y != 0.0f)
+        if (tempaxis.x != 0.0f || tempaxis.y != 0.0f)
         {
             if (OnMove != null)
                 OnMove(tempaxis);
--- Assets/Scripts/ThirdPersonCam.cs
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -12,6 +12,16 @@ public class ThirdPersonCam : MonoBehaviour
     public const string ERROR_UN_BINDCAM = "ThirdPersonCam脚本没有绑定摄像机!";
     public const string ERROR_UN_PLAYER = "ThirdPersonCam脚本没有指定玩家";
 
+    /// <summary>
+    /// 摄像机的基础方向
+    /// </summary>
+    private Vector3 CamBaseAxis = Vector3.back;
+
+    /// <summary>
+    /// 摄像机和碰撞体的交点向摄像机的观察点移动的距离
+    /// </summary>
+    private float CollisionReturnDis = 0.5f;
+
     #endregion
 
     #region 变量
@@ -30,11 +40,6 @@ public class ThirdPersonCam : MonoBehaviour
     /// 角色中心点偏移
     /// </summary>
     public Vector3 mPivotOffset = new Vector3(0.0f, 1.0f, 0.0f);
-
-    /// <summary>
-    /// 摄像机偏移
-    /// </summary>
-    public Vector3 mCamOffset = new Vector3(0.0f, 0.7f, -5.0f);
 
     /// <summary>
     /// 水平瞄准速度
@@ -69,7 +74,7 @@ public class ThirdPersonCam : MonoBehaviour
     /// <summary>
     /// 镜头推进的速度
     /// </summary>
-    public float mZoomSpeed = 2.0f;
+    public float mZoomSpeed = 1.0f;
 
     /// <summary>
     /// 水平旋转的角度
@@ -79,23 +84,17 @@ public class ThirdPersonCam : MonoBehaviour
     /// <summary>
     /// 垂直旋转的角度
     /// </summary>
-    private float mAngleV = 0.0f;
+    private float mAngleV = -30.0f;
 
     /// <summary>
     /// 基础摄像机偏移的倍率
     /// </summary>
     private float mDistance = 0.0f;
 
-#if USEINPUTSYSTEM
-    private Vector2 mTouchMove = Vector2.zero;
-
-    private Vector2 mTouchLastPos = Vector2.zero;
-#else
     /// <summary>
     /// 控制摄像机的ui
     /// </summary>
     public JoystickCamUI mJoystickCamUI;
-#endif
 
     #endregion
 
@@ -115,7 +114,8 @@ public class ThirdPersonCam : MonoBehaviour
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 
     }
 
@@ -140,13 +140,34 @@ public class ThirdPersonCam : MonoBehaviour
         }
 
         mAngleV = Mathf.Clamp(mAngleV, mMinVerticalAngle, mMaxVerticalAngle);
+        mDistance = Mathf.Clamp(mDistance, mMinDistance, mMaxDistance);
 
         Quaternion animRotation = Quaternion.Euler(-mAngleV, mAngleH, 0.0f);
         Quaternion camYRotation = Quaternion.Euler(0.0f, mAngleH, 0.0f);
-        mDistance = Mathf.Clamp(mDistance, mMinDistance, mMaxDistance);
-
         mCamera.rotation = animRotation;
-        mCamera.position = mPlayer.position + camYRotation * mPivotOffset + animRotation * mCamOffset * mDistance;
+
+        Vector3 lookatpos = mPlayer.position + camYRotation * mPivotOffset;
+        Vector3 camdir = animRotation * CamBaseAxis;
+        camdir.Normalize();
+        mCamera.position = lookatpos + camdir * mDistance;
+
+        // 计算碰撞后的摄像机点
+        RaycastHit rayhit;
+        bool hit = Physics.Raycast(lookatpos, camdir, out rayhit, mDistance);
+        if (hit)
+        {
+            // 屏蔽角色碰撞
+            bool charcol = rayhit.collider as CharacterController;
+            if (!charcol)
+            {
+                mCamera.position = rayhit.point - camdir * CollisionReturnDis;
+
+                // 距离修正在范围内(1, 避免摄像机穿插进入角色)
+                float distance = Vector3.Distance(mCamera.position, lookatpos);
+                distance = Mathf.Clamp(distance, mMinDistance, mMaxDistance);
+                mCamera.position = lookatpos + camdir * distance;
+            }
+        }
     }
 
 #endregion
@@ -161,7 +182,7 @@ public class ThirdPersonCam : MonoBehaviour
 
     private void OnJoystickCamPinch(float delta)
     {
-        mDistance += delta * mZoomSpeed;
+        mDistance -= delta * mZoomSpeed;
     }
 
     #endregion
